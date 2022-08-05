5de3dca61490d223ea79914351e3cb0a83b35c16
samiullah <samii51728@gmail.com>
2022-08-05 11:32:57 +0500

    connection updated

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const connectToDatabase = async () => {
   try {
     const connection = await mongoose.connect(process.env.DB_URI);
-    console.log("connected Successfully");
+    console.log("connected Successfully!!");
   } catch (err) {
     console.log("connection failed");
   }
